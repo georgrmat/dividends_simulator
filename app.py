bcfffb76398b652f2a42ed211713637343b6ee80
Georges <92797658+georgrmat@users.noreply.github.com>
2023-12-04 00:53:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from streamlit.components.v1 import components
+#import matplotlib.pyplot as plt
+import numpy as np
 
 # Code de calcul
 def simulate_investment(init_invest, alpha, nb_years, add_year, add_month):
@@ -26,40 +27,30 @@ def simulate_investment(init_invest, alpha, nb_years, add_year, add_month):
 st.title("Simulateur d'Investissement")
 
 # Choix de la langue
-language = st.radio("Choisissez la langue / Choose language:", ("Français", "English"))
+language = st.radio("Choose language:", ("Français", "English"))
 
 # Traduction
 if language == "Français":
-    st.write("Simulation d'évolution d'investissement sur N années.")
-    st.write("Entrez les paramètres ci-dessous :")
-    st.write("Investissement initial :")
-    init_invest = st.number_input("Montant initial :", min_value=0.0, value=1000.0)
-    st.write("Taux de croissance annuelle :")
-    alpha = st.number_input("Taux de croissance (en décimal) :", min_value=0.0, value=0.03)
-    st.write("Nombre d'années :")
-    nb_years = st.number_input("Nombre d'années :", min_value=1, value=100)
-    st.write("Contribution annuelle :")
-    add_year = st.number_input("Contribution annuelle :", min_value=0.0, value=100.0)
-    st.write("Contribution mensuelle :")
-    add_month = st.number_input("Contribution mensuelle :", min_value=0.0, value=0.0)
+    st.write("Simulation d'évolution d'investissement sur 100 ans.")
+    st.sidebar.write("Paramètres:")
+    init_invest = st.sidebar.number_input("Investissement initial :", min_value=0.0, value=1000.0)
+    alpha = st.sidebar.number_input("Taux de croissance annuelle :", min_value=0.0, value=0.03)
+    nb_years = st.sidebar.slider("Nombre d'années :", min_value=1, max_value=100, value=50)
+    add_year = st.sidebar.number_input("Contribution annuelle :", min_value=0.0, value=100.0)
+    add_month = st.sidebar.number_input("Contribution mensuelle :", min_value=0.0, value=0.0)
 
     # Calcul et affichage des résultats
     invested, evolution = simulate_investment(init_invest, alpha, nb_years, add_year, add_month)
     st.line_chart({"Investi": invested, "Valeur totale": evolution})
 
 elif language == "English":
-    st.write("Simulation of investment evolution over N years.")
-    st.write("Enter the parameters below:")
-    st.write("Initial investment:")
-    init_invest = st.number_input("Initial amount:", min_value=0.0, value=1000.0)
-    st.write("Annual growth rate:")
-    alpha = st.number_input("Growth rate (in decimal):", min_value=0.0, value=0.03)
-    st.write("Number of years:")
-    nb_years = st.number_input("Number of years:", min_value=1, value=100)
-    st.write("Annual contribution:")
-    add_year = st.number_input("Annual contribution:", min_value=0.0, value=100.0)
-    st.write("Monthly contribution:")
-    add_month = st.number_input("Monthly contribution:", min_value=0.0, value=0.0)
+    st.write("Simulation of investment evolution over 100 years.")
+    st.sidebar.write("Parameters:")
+    init_invest = st.sidebar.number_input("Initial investment:", min_value=0.0, value=1000.0)
+    alpha = st.sidebar.number_input("Annual growth rate:", min_value=0.0, value=0.03)
+    nb_years = st.sidebar.slider("Number of years:", min_value=1, max_value=100, value=50)
+    add_year = st.sidebar.number_input("Annual contribution:", min_value=0.0, value=100.0)
+    add_month = st.sidebar.number_input("Monthly contribution:", min_value=0.0, value=0.0)
 
     # Calculation and display of results
     invested, evolution = simulate_investment(init_invest, alpha, nb_years, add_year, add_month)
