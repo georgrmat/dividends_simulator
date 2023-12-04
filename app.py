e2f8081ddcc8f871f757708b3893108978756c92
Georges <92797658+georgrmat@users.noreply.github.com>
2023-12-04 01:11:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def simulate_investment(init_invest, alpha, nb_years, add_year, add_month):
         invested.append(invest)
 
         total += new
-        total *= (1 + alpha)
+        total *= (1 + alpha/10)
         evolution.append(total)
 
     return invested, evolution
@@ -34,7 +34,7 @@ if language == "Français":
     st.write("Simulation d'évolution d'investissement sur N années")
     st.sidebar.write("Paramètres")
     init_invest = st.sidebar.number_input("Investissement initial", min_value=0.0, value=1000.0)
-    alpha = st.sidebar.number_input("Taux de croissance annuelle", min_value=0.0, value=0.03)
+    alpha = st.sidebar.slider("Taux de croissance annuelle (%)", min_value=0, max_value=100, value = 3)
     nb_years = st.sidebar.slider("Nombre d'années", min_value=1, max_value=100, value=50)
     add_year = st.sidebar.number_input("Contribution annuelle", min_value=0.0, value=100.0)
     add_month = st.sidebar.number_input("Contribution mensuelle", min_value=0.0, value=0.0)
@@ -49,7 +49,7 @@ elif language == "English":
     st.write("Simulation of investment evolution over N years")
     st.sidebar.write("Parameters:")
     init_invest = st.sidebar.number_input("Initial investment", min_value=0.0, value=1000.0)
-    alpha = st.sidebar.number_input("Annual growth rate", min_value=0.0, value=0.03)
+    alpha = st.sidebar.slider("Annual growth percentage (%)", min_value=0, max_value = 100, value=100)
     nb_years = st.sidebar.slider("Number of years", min_value=1, max_value=100, value=50)
     add_year = st.sidebar.number_input("Annual contribution", min_value=0.0, value=100.0)
     add_month = st.sidebar.number_input("Monthly contribution", min_value=0.0, value=0.0)
